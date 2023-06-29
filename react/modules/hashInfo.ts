b3d407ae2273647bb091a97a26bb5c4d44a0ceec
Lesego Moruledi <lesegomoruledi@gmail.com>
2023-06-29 12:32:41 +0200

    updated checkout hashing

---
+++ react/modules/hashInfo.ts
@@ -0,0 +1,19 @@
+import { PixelMessage } from '../typings/events';
+import { toHash } from './analytics/utils';
+// import push from './push'
+
+export async function hashPersonalInfo(
+  e: PixelMessage
+) {
+
+    window.dataLayer.map((data: any, idx: number) => {
+        if (Object.keys(data).includes('visitorContactInfo'))
+            window.dataLayer[idx].visitorContactInfo = data?.visitorContactInfo.map((e: string) => toHash(e)
+            )
+        
+        if (Object.keys(data).includes('visitorDemographicInfo')) 
+            window.dataLayer[idx].visitorDemographicInfo = data?.visitorDemographicInfo.map((e: string) => toHash(e))
+        
+  })
+  
+}
