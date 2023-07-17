932f3d932d87d3e0cfc41c98a531b681e2a46635
Lesego Moruledi <37797756+lezegom@users.noreply.github.com>
2023-07-17 09:43:00 +0200

    Revert "BPF-1434"

--- react/index.tsx
+++ react/index.tsx
@@ -3,7 +3,6 @@ import { canUseDOM } from 'vtex.render-runtime'
 import { sendEnhancedEcommerceEvents } from './modules/enhancedEcommerceEvents'
 import { sendExtraEvents } from './modules/extraEvents'
 import { sendLegacyEvents } from './modules/legacyEvents'
-import { hashPersonalInfo } from './modules/hashInfo'
 import { PixelMessage } from './typings/events'
 
 // no-op for extension point
@@ -15,7 +14,6 @@ export function handleEvents(e: PixelMessage) {
   sendEnhancedEcommerceEvents(e)
   sendExtraEvents(e)
   sendLegacyEvents(e)
-  hashPersonalInfo(e)
 }
 
 if (canUseDOM) {
